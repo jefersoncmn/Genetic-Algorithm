9096d9ceb3ab18a6b3715a2bc86aa1e99ef9a075
jefersoncmn <jefersoncmn@hotmail.com>
2021-08-09 11:57:44 -0300

    feat: Read JSON fuctions done

--- src/leituraDeArquivo.py
+++ src/leituraDeArquivo.py
@@ -1,12 +1,17 @@
 import pandas as pd
 import json
 
-
 def ler_json(arq_json):
     with open(arq_json, 'r', encoding='utf8') as f:
         return json.load(f)
 
+def ler_materia_no_json(json_data, line):
+    materia = json_data[line]['id']
+    print(materia)
+    return materia
 
-def ler_segmento_no_json(json_data, line):
-    value = json_data[line].values()
-    print(value)
+def ler_dado_da_materia_no_json(json_data, line, key):
+    dado = json_data[line]['id']
+    print(dado)
+    return dado
+
